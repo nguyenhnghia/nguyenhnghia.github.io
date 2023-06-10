d1e4169c86441cb1e26eb3e7d2f154ed651757c9
nhnghia <nhnghia.dev@gmail.com>
2023-06-10 17:22:40 +0700

    chore: update content

--- src/components/sections/contact/index.jsx
+++ src/components/sections/contact/index.jsx
@@ -7,102 +7,96 @@ import Text from "../../_common/text";
 import List from "../../_common/list";
 
 import {
-	RiCodepenFill,
-	RiFacebookFill,
-	RiMailLine,
-	RiLinkedinFill,
-	RiGithubFill,
+  RiCodepenFill,
+  RiFacebookFill,
+  RiMailLine,
+  RiLinkedinFill,
+  RiGithubFill,
 } from "react-icons/ri";
 
 import "./scss/contact.shared.scss";
 import "./scss/contact.dark.scss";
 import "./scss/contact.light.scss";
 class contactSection extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			contactLinks: [
-				{
-					icon: <RiMailLine />,
-					href: "mailto:nhnghia.dev@gmail.com?&subject=Job opportunity",
-				},
-				{
-					icon: <RiFacebookFill />,
-					href: "https://www.facebook.com/nhnghia.dev/",
-				},
-				{
-					icon: <RiLinkedinFill />,
-					href: "https://www.linkedin.com/in/ngh%C4%A9a-nguy%E1%BB%85n-988900216/",
-				},
-				{
-					icon: <RiCodepenFill />,
-					href: "https://codepen.io/nhnghia-dev",
-				},
-				{
-					icon: <RiGithubFill />,
-					href: "https://github.com/nguyenhnghia",
-				},
-			],
-		};
-	}
-	render() {
-		return (
-			<DarkModeContext.Consumer>
-				{({ darkModeState }) => {
-					return (
-						<Section
-							extraClasses={
-								darkModeState
-									? `section-dark ${this.props.blockClassName}`
-									: `section-light ${this.props.blockClassName}`
-							}>
-							<Text
-								extraClasses={`${this.props.blockClassName}_title`}>
-								Looking for a<br /> front-end fresher
-							</Text>
-							<Text
-								extraClasses={`${this.props.blockClassName}_paragraph`}>
-								To land my first job, I'm currently seeking for
-								a front-end fresher position. So if you are
-								looking for a front-end fresher, why not{" "}
-								<Button
-									clicked={() =>
-										window.open(
-											`${process.env.PUBLIC_URL}/files/NguyenHuuNghia.Resume.en.pdf`
-										)
-									}>
-									check my resume
-								</Button>{" "}
-								and give us a chance to work together ?
-							</Text>
-							<List
-								extraClasses={`${this.props.blockClassName}_list`}>
-								{this.state.contactLinks.map((item, index) => {
-									return (
-										<Button
-											key={index}
-											itemExtraClasses={`tst-dl-${
-												this.state.contactLinks.length -
-												index
-											}x`}
-											clicked={() => {
-												window.open(
-													item.href,
-													"_blank"
-												);
-											}}>
-											<span className="icon">
-												{item.icon}
-											</span>
-										</Button>
-									);
-								})}
-							</List>
-						</Section>
-					);
-				}}
-			</DarkModeContext.Consumer>
-		);
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      contactLinks: [
+        {
+          icon: <RiMailLine />,
+          href: "mailto:nhnghia.dev@gmail.com?&subject=Job opportunity",
+        },
+        {
+          icon: <RiFacebookFill />,
+          href: "https://www.facebook.com/nhnghia.dev/",
+        },
+        {
+          icon: <RiLinkedinFill />,
+          href: "https://www.linkedin.com/in/ngh%C4%A9a-nguy%E1%BB%85n-988900216/",
+        },
+        {
+          icon: <RiCodepenFill />,
+          href: "https://codepen.io/nhnghia-dev",
+        },
+        {
+          icon: <RiGithubFill />,
+          href: "https://github.com/nguyenhnghia",
+        },
+      ],
+    };
+  }
+  render() {
+    return (
+      <DarkModeContext.Consumer>
+        {({ darkModeState }) => {
+          return (
+            <Section
+              extraClasses={
+                darkModeState
+                  ? `section-dark ${this.props.blockClassName}`
+                  : `section-light ${this.props.blockClassName}`
+              }
+            >
+              <Text extraClasses={`${this.props.blockClassName}_title`}>
+                Looking for a<br /> front-end developer
+              </Text>
+              <Text extraClasses={`${this.props.blockClassName}_paragraph`}>
+                To land my first job, I'm currently seeking for a front-end
+                developer position. So if you are looking for a front-end
+                developer, why not{" "}
+                <Button
+                  clicked={() =>
+                    window.open(
+                      `${process.env.PUBLIC_URL}/files/NguyenHuuNghia.Resume.en.pdf`
+                    )
+                  }
+                >
+                  check my resume
+                </Button>{" "}
+                and give us a chance to work together ?
+              </Text>
+              <List extraClasses={`${this.props.blockClassName}_list`}>
+                {this.state.contactLinks.map((item, index) => {
+                  return (
+                    <Button
+                      key={index}
+                      itemExtraClasses={`tst-dl-${
+                        this.state.contactLinks.length - index
+                      }x`}
+                      clicked={() => {
+                        window.open(item.href, "_blank");
+                      }}
+                    >
+                      <span className="icon">{item.icon}</span>
+                    </Button>
+                  );
+                })}
+              </List>
+            </Section>
+          );
+        }}
+      </DarkModeContext.Consumer>
+    );
+  }
 }
 export default contactSection;
--- src/components/sections/home/index.jsx
+++ src/components/sections/home/index.jsx
@@ -12,58 +12,54 @@ import "./scss/home.dark.scss";
 import "./scss/home.light.scss";
 
 class homeSection extends React.Component {
-	render() {
-		return (
-			<DarkModeContext.Consumer>
-				{({ darkModeState }) => {
-					return (
-						<Section
-							extraClasses={
-								darkModeState
-									? `section-dark ${this.props.blockClassName}`
-									: `section-light ${this.props.blockClassName}`
-							}>
-							<Text
-								extraClasses={`${this.props.blockClassName}_title`}>
-								<sub>I'm a</sub>
-								<h1>Frontend Developer</h1>
-							</Text>
-							<Text
-								extraClasses={`${this.props.blockClassName}_content`}>
-								Hello, I'm Nguyen Huu Nghia, I do things to make
-								websites look nice and easy to use.
-								<br /> A front-end fresher with serious passion
-								for front-end development and UI/UX.
-							</Text>
-							<div
-								className={`${this.props.blockClassName}_buttonGroup`}>
-								<Button
-									extraClasses="button-cta"
-									clicked={() =>
-										window.open(
-											`${process.env.PUBLIC_URL}/files/NguyenHuuNghia.Resume.en.pdf`
-										)
-									}>
-									<Text>read my resume</Text>
-								</Button>
-								<Button
-									extraClasses="button-ghost"
-									clicked={() =>
-										this.props.navigate(
-											this.props.sections[2].id
-										)
-									}>
-									<Text>
-										see my works
-										<RiArrowRightLine />
-									</Text>
-								</Button>
-							</div>
-						</Section>
-					);
-				}}
-			</DarkModeContext.Consumer>
-		);
-	}
+  render() {
+    return (
+      <DarkModeContext.Consumer>
+        {({ darkModeState }) => {
+          return (
+            <Section
+              extraClasses={
+                darkModeState
+                  ? `section-dark ${this.props.blockClassName}`
+                  : `section-light ${this.props.blockClassName}`
+              }
+            >
+              <Text extraClasses={`${this.props.blockClassName}_title`}>
+                <sub>I'm a</sub>
+                <h1>Frontend Developer</h1>
+              </Text>
+              <Text extraClasses={`${this.props.blockClassName}_content`}>
+                Hello, I'm Nguyen Huu Nghia, I do things to make websites look
+                nice and easy to use.
+                <br /> A front-end developer with serious passion for front-end
+                development and UI/UX.
+              </Text>
+              <div className={`${this.props.blockClassName}_buttonGroup`}>
+                <Button
+                  extraClasses="button-cta"
+                  clicked={() =>
+                    window.open(
+                      `${process.env.PUBLIC_URL}/files/NguyenHuuNghia.Resume.en.pdf`
+                    )
+                  }
+                >
+                  <Text>read my resume</Text>
+                </Button>
+                <Button
+                  extraClasses="button-ghost"
+                  clicked={() => this.props.navigate(this.props.sections[2].id)}
+                >
+                  <Text>
+                    see my works
+                    <RiArrowRightLine />
+                  </Text>
+                </Button>
+              </div>
+            </Section>
+          );
+        }}
+      </DarkModeContext.Consumer>
+    );
+  }
 }
 export default homeSection;
